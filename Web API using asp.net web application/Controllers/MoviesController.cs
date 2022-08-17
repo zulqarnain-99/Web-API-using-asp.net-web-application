feec1b7c4ff0c9cd08b02f4a1aeb3c6554180c50
Zulqarnain Qasim <zulqarnain.qasim@outlook.com>
2022-08-18 02:13:57 +0500

    Controlle created  for move

---
+++ Web API using asp.net web application/Controllers/MoviesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using Web_API_using_asp.net_web_application.Models;
+
+namespace Web_API_using_asp.net_web_application.Controllers
+{
+    public class MoviesController : ApiController
+    {
+        Movie[] movies = new Movie[]
+        {
+            new Movie { Id = 1 , Name = "Titanic", Genre= "Drama", Price = 9},
+            new Movie { Id = 1 , Name = "Titanic", Genre= "Drama", Price = 9},
+            new Movie { Id = 1 , Name = "Titanic", Genre= "Drama", Price = 9}
+        };
+        // GET: Movies
+        public IEnumerable<Movie> GetAllMovies()
+        {
+            return movies;  
+        }
+
+
+        public IHttpActionResult GetMovie(int id)
+        {
+            var movie = movies.FirstOrDefault((p) => p.Id == id);
+            if(movie == null)
+            {
+                return NotFound();
+            }
+            return Ok(movie);
+        }
+    }
+}
